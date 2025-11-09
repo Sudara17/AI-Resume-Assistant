64c45ab99cadf4d49157d0637cdb6e290766642f
Sudara17 <sudharshinisudara@gmail.com>
2025-11-09 21:56:22 +0530

    groq_llm.py

--- app/groq_llm.py
+++ app/groq_llm.py
@@ -2,9 +2,4 @@ from langchain_groq import ChatGroq
 import os
 
 def get_groq_llm(model="llama-3.1-8b-instant"):
-    return ChatGroq(
-        temperature=0.3,
-        model_name=model,
-        api_key=os.environ.get("GROQ_API_KEY")
-    )
-
+    return ChatGroq(temperature=0.3, model=model, api_key=os.environ.get("GROQ_API_KEY"), max_retries=2)
